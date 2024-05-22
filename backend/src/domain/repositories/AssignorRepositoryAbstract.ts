61934b8c76e9cbf8f19a29c3dd8d71d31aa72864
Deivison Isidoro <deivisonisidoro@gmail.com>
2024-05-22 20:32:28 -0300

    feat: creating assignor repository abstract class

---
+++ backend/src/domain/repositories/AssignorRepositoryAbstract.ts
@@ -0,0 +1,40 @@
+import { AssignorEntity } from "../entities/assignor/AssignorEntity";
+import { ReadAssignorDTO } from "../dtos/ReadAssignorDTO";
+
+/**
+ * Abstract class representing the repository for Assignor entities.
+ */
+export abstract class AssignorRepositoryAbstract {
+  /**
+   * Saves an assignor entity to the repository.
+   *
+   * @param {AssignorEntity} assignor - The assignor entity to save.
+   * @returns {Promise<Either<ValidationError, AssignorEntity>>} A promise that resolves to either a ValidationError or the saved AssignorEntity.
+   */
+  abstract create(assignor: AssignorEntity): Promise<ReadAssignorDTO>;
+
+
+  /**
+   * Finds an assignor entity by its ID.
+   *
+   * @param {string} id - The ID of the assignor to find.
+   * @returns {Promise<Either<ValidationError, AssignorEntity>>} A promise that resolves to either a ValidationError or the found AssignorEntity.
+   */
+  abstract findById(id: string): Promise<ReadAssignorDTO | undefined>;
+
+  /**
+   * Updates an assignor entity in the repository.
+   *
+   * @param {AssignorEntity} assignor - The assignor entity to update.
+   * @returns {Promise<Either<ValidationError, AssignorEntity>>} A promise that resolves to either a ValidationError or the updated AssignorEntity.
+   */
+  abstract update(assignor: AssignorEntity): Promise<ReadAssignorDTO>;
+
+  /**
+   * Deletes an assignor entity by its ID.
+   *
+   * @param {string} id - The ID of the assignor to delete.
+   * @returns {Promise<Either<ValidationError, void>>} A promise that resolves to either a ValidationError or void if successful.
+   */
+  abstract deleteById(id: string): Promise<void>;
+}
