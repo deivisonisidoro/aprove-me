2bf0e04699adc4f7c45eaaa97bc9e7d2e83bdb9f
Deivison Isidoro <deivisonisidoro@gmail.com>
2024-05-23 09:27:07 -0300

    test: testing read assignor use case

---
+++ backend/src/application/useCases/assignor/read/ReadAssignorUseCase.spec.ts
@@ -0,0 +1,53 @@
+import { ReadAssignorUseCaseAbstract } from "../../../../domain/useCases/read/ReadAssignorUseCaseAbstract";
+import { ReadAssignorDTO } from "../../../../domain/dtos/ReadAssignorDTO";
+import { ValidationError } from "../../../../domain/errors/ValidationErros";
+import { AssignorRepositoryAbstract } from "../../../../domain/repositories/AssignorRepositoryAbstract";
+import { ReadAssignorUseCase } from "./ReadAssignorUseCase";
+
+
+describe("ReadAssignorUseCase", () => {
+  let assignorRepositoryMock: AssignorRepositoryAbstract;
+  let readAssignorUseCase: ReadAssignorUseCaseAbstract;
+  const readAssignorDTO: ReadAssignorDTO = {
+    document: '123456789012345678901234567890',
+    email: 'test@example.com',
+    name: 'Test User',
+    phone: '1234567890',
+    login: 'testuser',
+  };
+
+  beforeEach(() => {
+    assignorRepositoryMock = {
+      create: jest.fn(),
+      findById: jest.fn(),
+      update: jest.fn(),
+      deleteById: jest.fn(),
+    } as unknown as AssignorRepositoryAbstract;
+
+
+    readAssignorUseCase = new ReadAssignorUseCase(
+      assignorRepositoryMock,
+
+    );
+  });
+  it("should return a Right with the read assignor DTO if assignor is found", async () => {
+    const assignorId = "validId";
+    (assignorRepositoryMock.findById as jest.Mock).mockResolvedValue(readAssignorDTO);
+    const result = await readAssignorUseCase.execute(assignorId);
+
+    expect(result.isRight()).toBe(true);
+    expect(result.value).toEqual(readAssignorDTO);
+  });
+
+  it("should return a Left with a validation error if assignor is not found", async () => {
+    const assignorId = "invalidId";
+
+    const result = await readAssignorUseCase.execute(assignorId);
+
+    expect(result.isLeft()).toBe(true);
+    expect(result.value).toBeInstanceOf(ValidationError);
+    if(result.isLeft()){
+      expect(result.value.message).toBe("Assignor not found.");
+    }
+  });
+});
