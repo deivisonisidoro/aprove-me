a124e1497cabb676b40d67b9c49e6d43c7162967
Deivison Isidoro <deivisonisidoro@gmail.com>
2024-05-23 17:59:07 -0300

    feat: adding test for updateAssignorEntity

--- backend/src/domain/entities/assignor/AssignorEntityFactory.spec.ts
+++ backend/src/domain/entities/assignor/AssignorEntityFactory.spec.ts
@@ -111,5 +111,102 @@ describe('AssignorEntityFactory', () => {
         );
       }
     });
+
+    it('should return a Left with ValidationError if the email format is invalid', () => {
+      const id = '123e4567-e89b-12d3-a456-426614174000';
+      const document = '1234567890';
+      const email = 'invalid-email'; // Invalid email format
+      const name = 'John Doe';
+      const phone = '1234567890';
+      const login = 'john_doe';
+      const password = 'P@ssword123';
+
+      const result = AssignorEntityFactory.createAssignorEntity(
+        id,
+        document,
+        email,
+        name,
+        phone,
+        login,
+        password,
+      );
+
+      expect(result.isLeft()).toBe(true);
+      if (result.isLeft()) {
+        expect(result.value).toBeInstanceOf(ValidationError);
+        expect(result.value.message).toBe(ValidationMessages.INVALID_EMAIL_FORMAT);
+      }
+    });
+  });
+
+  describe('updateAssignorEntity', () => {
+    const assignor = new AssignorEntity(
+      '123e4567-e89b-12d3-a456-426614174000',
+      '1234567890',
+      'test@example.com',
+      'John Doe',
+      '1234567890',
+      'validLogin',
+      'Valid1@Password',
+    );
+    it('should update an AssignorEntity instance with valid parameters', () => {
+      const document = 'new-1234567890';
+      const email = 'new-test@example.com';
+      const name = 'new-John Doe';
+      const phone = 'new-1234567890';
+      const login = 'new-john_doe';
+      const password = 'new-P@ssword123';
+
+      const result = AssignorEntityFactory.updateAssignorEntity(
+        assignor,
+        document,
+        email,
+        name,
+        phone,
+        login,
+        password,
+      );
+
+      expect(result.isRight()).toBe(true);
+      if (result.isRight()) {
+        expect(result.value).toBeInstanceOf(AssignorEntity);
+      }
+    });
+
+    it('should return a Left with ValidationError if updating with invalid email format', () => {
+      const email = 'invalid-email';
+
+      const result = AssignorEntityFactory.updateAssignorEntity(assignor, undefined, email);
+
+      expect(result.isLeft()).toBe(true);
+      if (result.isLeft()) {
+        expect(result.value).toBeInstanceOf(ValidationError);
+        expect(result.value.message).toBe(ValidationMessages.INVALID_EMAIL_FORMAT);
+      }
+    });
+
+    it('should return a Left with ValidationError if updating with invalid password length', () => {
+      const password = 'short';
+
+      const result = AssignorEntityFactory.updateAssignorEntity(assignor, undefined, undefined, undefined, undefined, undefined, password);
+
+      expect(result.isLeft()).toBe(true);
+      if (result.isLeft()) {
+        expect(result.value).toBeInstanceOf(ValidationError);
+        expect(result.value.message).toBe(ValidationMessages.PASSWORD_LENGTH_INVALID);
+      }
+    });
+
+    it('should return a Left with ValidationError if updating with invalid password complexity', () => {
+      const password = 'simplepassword';
+
+      const result = AssignorEntityFactory.updateAssignorEntity(assignor, undefined, undefined, undefined, undefined, undefined, password);
+
+      expect(result.isLeft()).toBe(true);
+      if (result.isLeft()) {
+        expect(result.value).toBeInstanceOf(ValidationError);
+        expect(result.value.message).toBe(ValidationMessages.PASSWORD_COMPLEXITY_INVALID);
+      }
+    });
   });
 });
