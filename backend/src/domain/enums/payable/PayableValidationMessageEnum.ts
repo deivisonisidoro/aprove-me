c9c104e5a95de6b0830dba1d0b8be97262e9150c
Deivison Isidoro <deivisonisidoro@gmail.com>
2024-05-24 08:19:15 -0300

    feat: creating new enum for success

---
+++ backend/src/domain/enums/payable/PayableValidationMessageEnum.ts
@@ -0,0 +1,9 @@
+/**
+ * Enum for validation messages.
+ *
+ * @enum {string}
+ */
+export enum PayableValidationMessages {
+  /** Error message for invalid UUID format. */
+  INVALID_UUID = 'Invalid UUID format.',
+}
