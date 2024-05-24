6d8e207824e5caa34e3166934cd66121e7e3b2d8
Deivison Isidoro <deivisonisidoro@gmail.com>
2024-05-24 17:08:59 -0300

    feat: creating payable dto

---
+++ backend/src/domain/dtos/payable/ReadPayableDTO.ts
@@ -0,0 +1,19 @@
+export class ReadPayableDTO {
+  id: string;
+  value: number;
+  emissionDate: Date;
+  assignorId: string;
+
+  /**
+   * Creates a CreatePayableDTO instance.
+   * @param {string | undefined} id - The unique identifier for the payable.
+   * @param {number} value - The value of the payable.
+   * @param {Date} emissionDate - The emission date of the payable.
+   * @param {string} assignorId - The id of the payable.
+   */
+  constructor(id: string | undefined, value: number, emissionDate: Date, assignorId: string) {
+    this.value = value;
+    this.emissionDate = emissionDate;
+    this.assignorId = assignorId;
+  }
+}
