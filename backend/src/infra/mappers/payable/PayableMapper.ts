938b40af2058b722e2a7205b2c9ec6fc4cbb12f6
Deivison Isidoro <deivisonisidoro@gmail.com>
2024-05-25 19:26:05 -0300

    feat: adding payable mapper

---
+++ backend/src/infra/mappers/payable/PayableMapper.ts
@@ -0,0 +1,39 @@
+import { Injectable } from "@nestjs/common";
+import { ReadPayableDTO } from "../../../domain/dtos/payable/ReadPayableDTO";
+import { PayableEntity } from "../../../domain/entities/payable/PayableEntity";
+
+@Injectable()
+export class PayableMapper {
+  /**
+   * Converts a PayableEntity to a ReadPayableDTO.
+   *
+   * @param {PayableEntity} entity - The payable entity to convert.
+   * @returns {ReadPayableDTO} The converted ReadPayableDTO.
+   */
+  toDTO(entity: PayableEntity): ReadPayableDTO {
+    
+    const dto = new ReadPayableDTO(
+      entity.id,
+      entity.value,
+      entity.emissionDate,
+      entity.assignorId,
+    );
+    return dto;
+  }
+
+  /**
+   * Converts a ReadPayableDTO to a PayableEntity.
+   *
+   * @param {ReadPayableDTO} dto - The payable DTO to convert.
+   * @returns {PayableEntity} The converted PayableEntity.
+   */
+  toEntity(dto: ReadPayableDTO): PayableEntity {
+    const entity = new PayableEntity(
+      dto.value,
+      dto.emissionDate,
+      dto.assignorId,
+      dto.id
+    );
+    return entity;
+  }
+}
