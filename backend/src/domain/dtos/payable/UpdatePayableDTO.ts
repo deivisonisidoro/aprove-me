f87e4b25e39cb47e471d81be3535e0144c918e77
Deivison Isidoro <deivisonisidoro@gmail.com>
2024-05-25 10:33:03 -0300

    feat: adding update payable dto

---
+++ backend/src/domain/dtos/payable/UpdatePayableDTO.ts
@@ -0,0 +1,25 @@
+export class UpdatePayableDTO {
+  id?: string;
+  value?: number;
+  emissionDate?: Date;
+  assignorId?: string;
+
+  /**
+   * Creates a UpdatePayableDTO instance.
+   * @param {string | undefined} id - The unique identifier for the payable.
+   * @param {number} value - The value of the payable.
+   * @param {Date} emissionDate - The emission date of the payable.
+   * @param {string} assignorId - The id of the payable.
+   */
+  constructor(
+    id: string | undefined,
+    value: number,
+    emissionDate: Date,
+    assignorId: string,
+  ) {
+    this.id = id;
+    this.value = value;
+    this.emissionDate = emissionDate;
+    this.assignorId = assignorId;
+  }
+}
