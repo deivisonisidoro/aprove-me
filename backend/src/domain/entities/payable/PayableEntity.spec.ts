bdc401b5e6520caacd5cec3617ac270ffe3a3d35
Deivison Isidoro <deivisonisidoro@gmail.com>
2024-05-25 19:24:39 -0300

    refactor: changing the variables name

--- backend/src/domain/entities/payable/PayableEntity.spec.ts
+++ backend/src/domain/entities/payable/PayableEntity.spec.ts
@@ -4,30 +4,30 @@ import { PayableEntity } from './PayableEntity';
 describe('PayableEntity', () => {
   describe('constructor', () => {
     it('should create a PayableEntity instance with valid parameters', () => {
-      const receivable = new PayableEntity(
+      const payable = new PayableEntity(
         100,
         new Date(),
         '123e4567-e89b-12d3-a456-426614174000',
       );
-      expect(receivable).toBeInstanceOf(PayableEntity);
+      expect(payable).toBeInstanceOf(PayableEntity);
     });
 
     it('should create a PayableEntity instance with an optional id', () => {
-      const receivable = new PayableEntity(
+      const payable = new PayableEntity(
         100,
         new Date(),
         '123e4567-e89b-12d3-a456-426614174000',
         '98765432-ABCD-ABCD-ABCD-ABCDEF012345',
       );
-      expect(receivable).toBeInstanceOf(PayableEntity);
+      expect(payable).toBeInstanceOf(PayableEntity);
     });
   });
 
   describe('getters and setters', () => {
-    let receivable: PayableEntity;
+    let payable: PayableEntity;
 
     beforeEach(() => {
-      receivable = new PayableEntity(
+      payable = new PayableEntity(
         100,
         new Date(),
         '123e4567-e89b-12d3-a456-426614174000',
@@ -36,17 +36,17 @@ describe('PayableEntity', () => {
     });
 
     it('should get the id', () => {
-      expect(receivable.id).toBe('98765432-ABCD-ABCD-ABCD-ABCDEF012345');
+      expect(payable.id).toBe('98765432-ABCD-ABCD-ABCD-ABCDEF012345');
     });
 
     it('should set the id', () => {
-      const result = receivable.setId('123e4567-e89b-12d3-a456-426614174000');
+      const result = payable.setId('123e4567-e89b-12d3-a456-426614174000');
       expect(result.isLeft()).toBe(false);
-      expect(receivable.id).toBe('123e4567-e89b-12d3-a456-426614174000');
+      expect(payable.id).toBe('123e4567-e89b-12d3-a456-426614174000');
     });
 
     it('should return a Left with ValidationError if the id is not a valid UUID', () => {
-      const result = receivable.setId('invalid-id');
+      const result = payable.setId('invalid-id');
       expect(result.isLeft()).toBe(true);
       if (result.isLeft()) {
         expect(result.value.message).toBe(
@@ -56,13 +56,13 @@ describe('PayableEntity', () => {
     });
 
     it('should allow setting the id to undefined', () => {
-      const result = receivable.setId(undefined);
+      const result = payable.setId(undefined);
       expect(result.isLeft()).toBe(false);
-      expect(receivable.id).toBeUndefined();
+      expect(payable.id).toBeUndefined();
     });
 
     it('should return a Left with ValidationError if the id is not a valid UUID when setting it to a non-undefined value', () => {
-      const result = receivable.setId('invalid-id');
+      const result = payable.setId('invalid-id');
       expect(result.isLeft()).toBe(true);
       if (result.isLeft()) {
         expect(result.value.message).toBe(
@@ -72,41 +72,41 @@ describe('PayableEntity', () => {
     });
 
     it('should get the value', () => {
-      expect(receivable.value).toBe(100);
+      expect(payable.value).toBe(100);
     });
 
     it('should set the value', () => {
-      receivable.value = 200;
-      expect(receivable.value).toBe(200);
+      payable.value = 200;
+      expect(payable.value).toBe(200);
     });
 
     it('should get the emission date', () => {
-      expect(receivable.emissionDate).toBeInstanceOf(Date);
+      expect(payable.emissionDate).toBeInstanceOf(Date);
     });
 
     it('should set the emission date', () => {
       const newDate = new Date();
-      receivable.emissionDate = newDate;
-      expect(receivable.emissionDate).toBe(newDate);
+      payable.emissionDate = newDate;
+      expect(payable.emissionDate).toBe(newDate);
     });
 
     it('should get the assignor', () => {
-      expect(receivable.assignorId).toBe(
+      expect(payable.assignorId).toBe(
         '123e4567-e89b-12d3-a456-426614174000',
       );
     });
 
     it('should set the assignorId', () => {
-      const result = receivable.setAssignorId(
+      const result = payable.setAssignorId(
         '123e4567-e89b-12d3-a456-426614174000',
       );
       expect(result.isLeft()).toBe(false);
-      expect(receivable.assignorId).toBe(
+      expect(payable.assignorId).toBe(
         '123e4567-e89b-12d3-a456-426614174000',
       );
     });
     it('should return a Left with ValidationError if the assignorId is not a valid UUID', () => {
-      const result = receivable.setAssignorId('invalid-id');
+      const result = payable.setAssignorId('invalid-id');
       expect(result.isLeft()).toBe(true);
       if (result.isLeft()) {
         expect(result.value.message).toBe(
