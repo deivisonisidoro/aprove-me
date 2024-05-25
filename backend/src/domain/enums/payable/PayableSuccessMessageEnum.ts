0ba8934f4f73aa19caabfd70c060da02e9c7d868
Deivison Isidoro <deivisonisidoro@gmail.com>
2024-05-25 11:12:21 -0300

    feat: creating new enum for success messages

---
+++ backend/src/domain/enums/payable/PayableSuccessMessageEnum.ts
@@ -0,0 +1,9 @@
+/**
+ * Enum for success messages.
+ *
+ * @enum {string}
+ */
+export enum PayableSuccessMessage {
+  /** Error message for invalid UUID format. */
+  DELETED_SUCCESSFULLY = 'Payable deleted successfully.',
+}
