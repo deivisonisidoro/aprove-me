83d8747db4d0ea7bf30f2ef98c296788b5849922
Deivison Isidoro <deivisonisidoro@gmail.com>
2024-05-29 09:20:01 -0300

    feat: adding Injectable decorator

--- backend/src/infra/mappers/payable/PayableMapper.ts
+++ backend/src/infra/mappers/payable/PayableMapper.ts
@@ -1,6 +1,9 @@
+import { Injectable } from '@nestjs/common';
+
 import { ReadPayableDTO } from '../../../domain/dtos/payable/ReadPayableDTO';
 import { PayableEntity } from '../../../domain/entities/payable/PayableEntity';
 
+@Injectable()
 export class PayableMapper {
   /**
    * Converts a PayableEntity to a ReadPayableDTO.
@@ -8,14 +11,16 @@ export class PayableMapper {
    * @param {PayableEntity} entity - The payable entity to convert.
    * @returns {ReadPayableDTO} The converted ReadPayableDTO.
    */
-  toDTO(entity: PayableEntity): ReadPayableDTO {
-    const dto = new ReadPayableDTO(
-      entity.id,
-      entity.value,
-      entity.emissionDate,
-      entity.assignorId,
-    );
-    return dto;
+  toDTO(entity: PayableEntity): ReadPayableDTO | undefined {
+    if (entity) {
+      const dto = new ReadPayableDTO(
+        entity.id,
+        entity.value,
+        entity.emissionDate,
+        entity.assignorId,
+      );
+      return dto;
+    }
   }
 
   /**
@@ -24,13 +29,17 @@ export class PayableMapper {
    * @param {ReadPayableDTO} dto - The payable DTO to convert.
    * @returns {PayableEntity} The converted PayableEntity.
    */
-  toEntity(dto: ReadPayableDTO): PayableEntity {
-    const entity = new PayableEntity(
-      dto.value,
-      dto.emissionDate,
-      dto.assignorId,
-      dto.id,
-    );
-    return entity;
+  toEntity(dto: any): PayableEntity | undefined {
+    if (dto !== null) {
+      const entity = new PayableEntity(
+        dto.value,
+        dto.emissionDate,
+        dto.assignorId,
+        dto.id,
+      );
+      return entity;
+    }
+
+    return null;
   }
 }
